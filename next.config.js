0950a581a612daa3ac85bbd56aa2c82c3e3ea521
fox <fox@vvfox.com>
2020-02-19 04:23:08 +0800

    tsx & mdx

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 const withSass = require('@zeit/next-sass')
+const rehypePrism = require('@mapbox/rehype-prism')
 
-module.exports = withSass({
-  webpack(config, option) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    rehypePlugins: [rehypePrism],
+    hastPlugins: [rehypePrism], // <-----------  now effect
   },
 })
+
+module.exports = withMDX(
+  withSass({
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'tsx'],
+    webpack(config, option) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
+      return config
+    },
+  })
+)
