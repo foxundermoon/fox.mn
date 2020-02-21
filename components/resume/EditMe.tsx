ee631624883fbbfcb221f967a6891a6d839749a1
fox <fox@vvfox.com>
2020-02-21 16:52:36 +0800

    fix editme

--- components/resume/EditMe.tsx
+++ components/resume/EditMe.tsx
@@ -1,30 +1,31 @@
 import EditIcon from '@material-ui/icons/Edit'
-import { Tooltip, IconButton, Link ,Fab, Theme} from '@material-ui/core'
-import {makeStyles , createStyles} from '@material-ui/core/styles'
+import { Tooltip, IconButton, Link, Fab, Theme } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+import { FunctionComponent } from 'react'
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    fab:{
+    fab: {
       position: 'fixed',
       bottom: theme.spacing(10),
       right: theme.spacing(6),
-    }
-  }),
-);
+    },
+  })
+)
 
-export default () => {
-
+const EditMe: FunctionComponent<{ f: string }> = ({ f }) => {
   const classes = useStyles()
   return (
     <Tooltip title="edit me @GitHub">
-      <Fab className={classes.fab} >
-      <Link
-        target="_blank"
-        href={`https://github.com/foxundermoon/fox.mn/edit/master/pages/br.mdx`}
-      >
-        <EditIcon />
-      </Link>
-
+      <Fab className={classes.fab}>
+        <Link
+          target="_blank"
+          href={`https://github.com/foxundermoon/fox.mn/edit/master/${f}`}
+        >
+          <EditIcon />
+        </Link>
       </Fab>
     </Tooltip>
   )
 }
+
+export default EditMe
